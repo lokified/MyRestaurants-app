f04a4bf26b5054f8ce92312ac785c39568509e7b
lokified <sheldon.obonyo@student.moringaschool.com>
2021-11-23 13:38:09 +0300

    add List view to restaurant activity

--- app/src/main/java/com/moringaschool/myrestaurants/RestaurantActivity.java
+++ app/src/main/java/com/moringaschool/myrestaurants/RestaurantActivity.java
@@ -4,17 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class RestaurantActivity extends AppCompatActivity {
 
     private TextView mLocationTextView;
+    private ListView mListView;
+    private String[] restaurants = new String[] {"Comrades","Kwa Mathe","Mi Mero Mole", "Mother's Bistro",
+            "Life of Pie", "Screen Door", "Luc Lac", "Sweet Basil",
+            "Slappy Cakes", "Equinox", "Miss Delta's", "Andina",
+            "Lardo", "Portland City Grill", "Fat Head's Brewery",
+            "Chipotle", "Subway"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant);
+
+        mListView = (ListView) findViewById(R.id.listView);
         mLocationTextView = (TextView) findViewById(R.id.locationTextView);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,restaurants);
+        mListView.setAdapter(adapter);
+
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
         mLocationTextView.setText("Here are all the restaurants near : " + location);
