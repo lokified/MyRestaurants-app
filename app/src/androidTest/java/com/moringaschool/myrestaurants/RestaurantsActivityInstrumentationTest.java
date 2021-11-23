1c2146bc3ec8bbf860d63de916930956498afe32
lokified <sheldon.obonyo@student.moringaschool.com>
2021-11-24 00:43:56 +0300

    add lists description with custom adapters

---
+++ app/src/androidTest/java/com/moringaschool/myrestaurants/RestaurantsActivityInstrumentationTest.java
@@ -0,0 +1,54 @@
+package com.moringaschool.myrestaurants;
+
+import android.view.View;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.not;
+
+@RunWith(AndroidJUnit4.class)
+public class RestaurantsActivityInstrumentationTest {
+
+    @Rule
+    public ActivityScenarioRule<RestaurantActivity> activityTestRule =
+            new ActivityScenarioRule<>(RestaurantActivity.class);
+
+    private View activityDecorView;
+
+    @Before
+    public void setUp() {
+        activityTestRule.getScenario().onActivity(new ActivityScenario.ActivityAction<RestaurantActivity>() {
+            @Override
+            public void perform(RestaurantActivity activity) {
+                activityDecorView = activity.getWindow().getDecorView();
+            }
+        });
+    }
+
+    @Test
+    public void listItemClickDisplaysToastWithCorrectRestaurant(){
+        String restaurantName = "Comrades";
+        onData(anything())
+                .inAdapterView(withId(R.id.listView))
+                .atPosition(0)
+                .perform(click());
+        onView(withText(restaurantName))
+                .inRoot(withDecorView(not(activityDecorView)))
+                .check(matches(withText(restaurantName)));
+    }
+}
--- app/src/main/java/com/moringaschool/myrestaurants/RestaurantActivity.java
+++ app/src/main/java/com/moringaschool/myrestaurants/RestaurantActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -11,34 +12,40 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class RestaurantActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
-    private TextView mLocationTextView;
-    private ListView mListView;
+public class RestaurantActivity extends AppCompatActivity {
+    public static final String TAG = RestaurantActivity.class.getSimpleName();
+
+    @BindView(R.id.locationTextView) TextView mLocationTextView;
+    @BindView(R.id.listView) ListView mListView;
     private String[] restaurants = new String[] {"Comrades","Kwa Mathe","Mi Mero Mole", "Mother's Bistro",
             "Life of Pie", "Screen Door", "Luc Lac", "Sweet Basil",
             "Slappy Cakes", "Equinox", "Miss Delta's", "Andina",
             "Lardo", "Portland City Grill", "Fat Head's Brewery",
             "Chipotle", "Subway"};
+    private String[] cuisines = new String[] {"matumbo","Vegan Food", "Breakfast", "Fishs Dishs", "Scandinavian",
+            "Coffee", "English Food", "Burgers", "Fast Food", "Noodle Soups", "Mexican", "BBQ", "Cuban",
+            "Bar Food", "Sports Bar", "Breakfast", "Mexican" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant);
+        ButterKnife.bind(this);
 
-        mListView = (ListView) findViewById(R.id.listView);
-        mLocationTextView = (TextView) findViewById(R.id.locationTextView);
-
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,restaurants);
+        MyRestaurantsArrayAdapter adapter = new MyRestaurantsArrayAdapter(this, android.R.layout.simple_list_item_1,restaurants,cuisines);
         mListView.setAdapter(adapter);
 
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long l) {
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
                 String restaurant = ((TextView)view).getText().toString();
                 Toast.makeText(RestaurantActivity.this, restaurant, Toast.LENGTH_LONG).show();
             }
         });
+        Log.d(TAG,"In the onCreate method");
 
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
